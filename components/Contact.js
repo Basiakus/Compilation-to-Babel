95e49e11755d2f1707c25824462d4f4ea08648a0
Adam Basiak <basiakus@gmail.com>
2017-12-14 00:02:01 +0100

    () after return and json setting

--- components/Contact.js
+++ components/Contact.js
@@ -4,8 +4,8 @@ var Contact = React.createClass({
   },
 
   render: function() {
-    return (
-      <div className={'contactItem'}>
+    return 
+      (<div className={'contactItem'}>
         <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
         <p className={'contactLabel'}>
             Imię: {this.props.contact.firstName}
@@ -13,7 +13,7 @@ var Contact = React.createClass({
         <a href={'mailto:' + this.props.item.email}>
             {this.props.item.email}
         </a>
-      </div>
-    )
+      </div>)
+    
   },
 });
