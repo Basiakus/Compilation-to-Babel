2a1561e991c7b979badfa5817a0d2191890c6609
Basiakus <panadambasiak@gmail.com>
2017-12-26 15:00:16 +0100

    fix

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,7 +1,7 @@
 var Contacts = React.createClass({
   propTypes: {
     items: React.PropTypes.array.isRequired,
-  }
+  },
 
   render: function() {
     var contacts = this.props.items.map(function(contact) {
