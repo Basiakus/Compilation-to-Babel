a94bbf38a0c8579bbd2984a3f55cac743c4bcc25
Adam Basiak <basiakus@gmail.com>
2018-02-06 22:29:17 +0100

    update

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,12 +1,12 @@
-var Contact = React.createClass({
+var Contacts = React.createClass({
 
 	render: function() {
-	    var contacts = this.props.items.map(function(contact) {
-	        return (<Contact item={contact} key={contact.id}></Contact>)
-	    });
-	    
-    return (
-    <ul className={'contactList'}>{contacts}</ul>
-    );
-  }
+		var contacts = this.props.items.map(function(contact) {
+			return (<Contact item={contact} key={contact.id}></Contact>)
+		});
+
+		return (
+			<ul className={'contactList'}>{contacts}</ul>
+		);
+	}
 });
